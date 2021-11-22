4e8307a3d515dd4fe098752c2f109946db47e7d3
18368404576@163.com <18368404576@163.com>
2021-11-22 19:28:20 +0800

    master

--- GIt_Test.cpp
+++ GIt_Test.cpp
@@ -1,6 +1,23 @@
 #include<stdio.h>
 int main(void)
 {
-    printf("Hello Worlde!\r");
-    printf("你好世界！");
+    /*\r:回车符,\n:换行符,\b:退格符*/
+    printf("1.正常输出,无回车,无换行,无回退\n");
+    printf("Hello Worlde!");
+    printf("你好世界！\n");
+    printf("2.回车输出(r),无换行,无回退\n");
+    printf("   Hello Worlde!\r");
+    printf("你好世界！\n");
+    printf("3.换行输出(n),无回车,无回退\n");
+    printf("Hello Worlde!\n");
+    printf("你好世界！\n");
+    printf("4.先回车+后换行输出(rn),无回退\n");
+    printf("Hello Worlde!\r\n");
+    printf("你好世界！\n");
+    printf("5.先换行+后回车输出(nr),无回退\n");
+    printf("Hello Worlde!\n\r");
+    printf("你好世界！\n");
+    printf("6.回退输出\n");
+    printf("Hello Worlde!\b");
+    printf("你好世界！\n");
 }
